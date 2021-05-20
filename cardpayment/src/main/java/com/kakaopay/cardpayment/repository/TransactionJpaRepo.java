73de47d545944719055c3afdfc07be32bcb5a0e4
pjygabon <84433869+pjygabon@users.noreply.github.com>
2021-05-21 00:02:03 +0900

    Delete cardpayment directory

--- cardpayment/src/main/java/com/kakaopay/cardpayment/repository/TransactionJpaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.kakaopay.cardpayment.repository;
-
-import com.kakaopay.cardpayment.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface TransactionJpaRepo extends JpaRepository<Transaction, Long> {
-
-}
